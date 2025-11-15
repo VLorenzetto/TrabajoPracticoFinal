923634731b05588a15f2b6621fa8e06ac06ca6a2
Valentino Lorenzetto <valentino.lorenzetto@est.fi.uncoma.edu.ar>
2025-11-15 14:01:48 -0300

    Avance TDAs

---
+++ Avion.java
@@ -0,0 +1,110 @@
+class Avion {
+    private String idAvion;
+    private String modeloAvion;
+    private int cantidadVuelos;
+    private int cantidadAsientos;
+    private int kmRecorridos;
+
+    // Constructures
+    public Avion(String idAvion, String modeloAvion, int cantidadVuelos, int cantidadAsientos,
+            int kmRecorridos) {
+        this.idAvion = idAvion;
+        this.modeloAvion = modeloAvion;
+        this.cantidadVuelos = cantidadVuelos;
+        this.cantidadAsientos = cantidadAsientos;
+        this.kmRecorridos = kmRecorridos;
+
+    }
+
+    public Avion(String idAvion) {
+        this.idAvion = idAvion;
+        modeloAvion = "X";
+        cantidadVuelos = 0;
+        cantidadAsientos = 0;
+        kmRecorridos = 0;
+    }
+
+    // Observadores
+    public String getIdAvion() {
+        return idAvion;
+    }
+
+    public String getModeloAvion() {
+        return modeloAvion;
+    }
+
+    public int getCantidadVuelos() {
+        return cantidadVuelos;
+    }
+
+    public int getCantidadAsientos() {
+        return cantidadAsientos;
+    }
+
+    public int getKmRecorridos() {
+        return kmRecorridos;
+    }
+
+    public String toString() {
+        return ("Avión: [" + idAvion + "], Modelo: " + modeloAvion + ", Cantidad de vuelos: "
+                + cantidadVuelos + ", Cantidad de asientos: " + cantidadAsientos 
+                + " Distancia recorrida: " + kmRecorridos+ " Km.");
+    }
+
+    public boolean equals(Avion otroAvion) {
+        return (idAvion == otroAvion.getIdAvion());
+    }
+
+    // Modificadores
+    public void setCantidadVuelos(int cantidadVuelos) {
+        this.cantidadVuelos = cantidadVuelos;
+    }
+
+    public void setDistanciaRecorrida(int distancia) {
+        this.kmRecorridos = distancia;
+    }
+
+    // Propias de la clase
+
+    public static boolean verificarIdAvion(String idAvion) {
+
+        boolean valido = false;
+
+        switch (idAvion.substring(0, 3)) {
+
+            case "LV-":
+                valido = idAvion.matches("LV-[A-Z]{3}") || // uso general
+                        idAvion.matches("LV-X[0-9]{3}") || // experimental
+                        idAvion.matches("LV-S[0-9]{3}") || // deportiva liviana
+                        idAvion.matches("LV-SX[0-9]{3}"); // deportiva liviana experimental
+                break;
+
+            case "LQ-":
+                valido = idAvion.matches("LQ-[A-Z]{3}"); // gubernamental
+                break;
+
+            default:
+                valido = false;
+        }
+
+        return valido;
+    }
+
+    public static void imprimirArrAviones(Avion[] arrAviones) {
+
+        System.out.println("ID        MODELO           VUELOS     ASIENTOS   KM RECORRIDOS");
+        System.out.println("-----------------------------------------------------------------------");
+
+        for (Avion datosAvion : arrAviones) {
+            if (datosAvion != null) {
+                System.out.printf("%-10s %-15s %-10d %-10d %-15d\n",
+                        datosAvion.getIdAvion(),
+                        datosAvion.getModeloAvion(),
+                        datosAvion.getCantidadVuelos(),
+                        datosAvion.getCantidadAsientos(),
+                        datosAvion.getKmRecorridos());
+            }
+        }
+    }
+
+}
---
+++ Ruta.java
@@ -0,0 +1,83 @@
+class Ruta {
+    private int numeroRuta;
+    private String ciudadOrigen;
+    private String ciudadDestino;
+    private int distancia;
+    private boolean esInternacional;
+
+    // Constructores
+    public Ruta(int numeroRuta, String ciudadOrigen, String ciudadDestino, int distancia, String esInternacional) {
+        this.numeroRuta = numeroRuta;
+        this.ciudadOrigen = ciudadOrigen;
+        this.ciudadDestino = ciudadDestino;
+        this.distancia = distancia;
+        this.esInternacional = Ruta.verificaInternacional(esInternacional);
+    }
+
+    public Ruta(int numeroRuta) {
+        this.numeroRuta = numeroRuta;
+        ciudadOrigen = "X";
+        ciudadDestino = "X";
+        distancia = 0;
+        esInternacional = false;
+    }
+
+    // Observadores
+
+    public int getNumeroRuta() {
+        return numeroRuta;
+    }
+
+    public String getCiudadOrigen() {
+        return ciudadOrigen;
+    }
+
+    public String getCiudadDestino() {
+        return ciudadDestino;
+    }
+
+    public int getDistancia() {
+        return distancia;
+    }
+
+    public boolean getEsInternacional() {
+        return esInternacional;
+    }
+
+    public String toString() {
+        return "Ruta Nº " + numeroRuta +
+                " | Origen: " + ciudadOrigen +
+                " | Destino: " + ciudadDestino +
+                " | Distancia: " + distancia + " km" +
+                " | " + (esInternacional ? "Si" : "No");
+    }
+
+    public boolean equals(Ruta otraRuta) {
+        return (numeroRuta == otraRuta.getNumeroRuta());
+    }
+
+    // Modificadores? (En mi parecer son todos datos fijos)
+
+    // Propias de la clase
+    public static boolean verificaInternacional(String internacional) {
+        return internacional != null && internacional.equalsIgnoreCase("SI");
+    }
+
+    public static void imprimirArrRutas(Ruta[] arrRutas) {
+
+        System.out.println("RUTA N°        ORIGEN           DESTINO     DISTANCIA   INTERNACIONAL");
+        System.out.println("-----------------------------------------------------------------------");
+
+        for (Ruta datosRuta : arrRutas) {
+            if (datosRuta != null) {
+                System.out.printf("%-10d %-15s %-15s %-10d %-15s\\n",
+                        datosRuta.getNumeroRuta(),
+                        datosRuta.getCiudadOrigen(),
+                        datosRuta.getCiudadDestino(),
+                        datosRuta.getDistancia(),
+                        datosRuta.getEsInternacional() ? "Si" : "No");
+            }
+        }
+    }
+
+}
